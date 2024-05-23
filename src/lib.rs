0c70fbbe090e95fea54a3e6d09d9b5e9832a5741
Robin Schroer <git@sulami.xyz>
2024-05-24 08:58:34 +0900

    Pivot to just the DShot frame
    DMA is too deeply embedded in specific platforms that it can't really be neatly provided in a generic way.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 //! Support for the DShot ESC protocol
 //!
+//! This crate provides
+//!
 //! DShot has two-byte frames, where the first 11 bits are the throttle speed, bit 12 is a
 //! telemetry request flag, and the last four bits are a checksum.
 //!
@@ -10,233 +12,76 @@
 
 // TODO Special commands
 //      Bidirectional DShot
-//      embedded-hal-async
 
 #![no_std]
 
-use core::fmt::{Debug, Formatter};
-use core::time::Duration;
-
-use embedded_hal::{delay::DelayNs, digital::OutputPin};
-
-/// How long to stay low between frames.
-///
-/// This is actually just a lower bound.
-pub const INTER_FRAME_PAUSE: Duration = Duration::from_micros(2);
-
-/// The protocol speed.
-pub enum Speed {
-    DShot150,
-    DShot300,
-    DShot600,
-    DShot1200,
-}
-
-impl Speed {
-    /// The time to hold the pin high for a set bit.
-    #[inline]
-    fn one_hold_time(&self) -> Duration {
-        match self {
-            Self::DShot150 => Duration::from_nanos(5_000),
-            Self::DShot300 => Duration::from_nanos(2_500),
-            Self::DShot600 => Duration::from_nanos(1_250),
-            Self::DShot1200 => Duration::from_nanos(625),
-        }
-    }
-
-    /// The time to hold the pin high for an unset bit.
-    #[inline]
-    fn zero_hold_time(&self) -> Duration {
-        match self {
-            Self::DShot150 => Duration::from_nanos(2_500),
-            Self::DShot300 => Duration::from_nanos(1_250),
-            Self::DShot600 => Duration::from_nanos(625),
-            Self::DShot1200 => Duration::from_nanos(313),
-        }
-    }
-
-    /// The total time of one bit, both high and low.
-    #[inline]
-    fn bit_time(&self) -> Duration {
-        match self {
-            Self::DShot150 => Duration::from_nanos(6_667),
-            Self::DShot300 => Duration::from_nanos(3_333),
-            Self::DShot600 => Duration::from_nanos(1_667),
-            Self::DShot1200 => Duration::from_nanos(833),
-        }
-    }
-}
-
-/// An error that occurred during DShot operation.
-pub enum Error<PIN>
-where
-    PIN: OutputPin,
-    PIN::Error: Debug,
-{
-    /// The provided throttle value was greater than 1999.
-    OutOfBoundsError,
-    /// A wrapped error occurred while trying to pull the pin high or low.
-    PinError(PIN::Error),
-}
-
-impl<PIN> core::fmt::Debug for Error<PIN>
-where
-    PIN: OutputPin,
-    PIN::Error: Debug,
-{
-    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
-        match self {
-            Self::OutOfBoundsError => f.write_str("OutOfBoundsError"),
-            Self::PinError(e) => f.write_fmt(format_args!("PinError<{e:?}>")),
-        }
-    }
-}
-
-/// A stateful DShot controller.
-pub struct DShotController<PIN, DELAY>
-where
-    PIN: OutputPin,
-    PIN::Error: Debug,
-    DELAY: DelayNs,
-{
-    pin: PIN,
-    delay: DELAY,
-    speed: Speed,
-    throttle: u16,
-    telemetry_enabled: bool,
-}
-
-impl<PIN, DELAY> DShotController<PIN, DELAY>
-where
-    PIN: OutputPin,
-    PIN::Error: Debug,
-    DELAY: DelayNs,
-{
-    /// Creates a new DShot controller.
-    ///
-    /// Throttle defaults to zero, telemetry to off.
-    pub fn new(pin: PIN, delay: DELAY, speed: Speed) -> Self {
-        Self {
-            pin,
-            delay,
-            speed,
-            throttle: 0,
-            telemetry_enabled: false,
-        }
-    }
-
-    /// Sets the throttle and sends it over the wire.
-    pub fn set_throttle(&mut self, throttle: u16) -> Result<(), Error<PIN>> {
-        if throttle >= 2000 {
-            return Err(Error::OutOfBoundsError);
-        }
-
-        self.send_throttle_command(throttle)?;
-        self.throttle = throttle;
-
-        Ok(())
-    }
-
-    /// Sends the current throttle over the wire.
-    ///
-    /// This method blocks until the entire frame is sent.
-    fn send_throttle_command(&mut self, throttle: u16) -> Result<(), Error<PIN>> {
-        let frame = Frame::new(throttle, self.telemetry_enabled);
-
-        let one_up_time = self.speed.one_hold_time();
-        let zero_up_time = self.speed.zero_hold_time();
-
-        let one_down_time = self.speed.bit_time() - one_up_time;
-        let zero_down_time = self.speed.bit_time() - zero_up_time;
-
-        for bit in frame.bits() {
-            if bit {
-                self.pin.set_high().map_err(Error::PinError)?;
-                self.delay.delay_ns(one_up_time.as_nanos() as u32);
-                self.pin.set_low().map_err(Error::PinError)?;
-                self.delay.delay_ns(one_down_time.as_nanos() as u32);
-            } else {
-                self.pin.set_high().map_err(Error::PinError)?;
-                self.delay.delay_ns(zero_up_time.as_nanos() as u32);
-                self.pin.set_low().map_err(Error::PinError)?;
-                self.delay.delay_ns(zero_down_time.as_nanos() as u32);
-            };
-        }
-
-        Ok(())
-    }
-
-    /// Enables telemetry requests.
-    ///
-    /// Resends the current throttle value to actually let the ESC know.
-    pub fn enable_telemetry(&mut self) -> Result<(), Error<PIN>> {
-        self.telemetry_enabled = true;
-        self.send_throttle_command(self.throttle)
-    }
-
-    /// Disables telemetry requests.
-    ///
-    /// Resends the current throttle value to actually let the ESC know.
-    pub fn disable_telemetry(&mut self) -> Result<(), Error<PIN>> {
-        self.telemetry_enabled = false;
-        self.send_throttle_command(self.throttle)
-    }
-}
-
 /// A frame of two bytes that get send over the wire.
-struct Frame(u16);
+pub struct Frame {
+    inner: u16,
+}
 
 impl Frame {
-    /// Creates a new frame with the given throttle (0-1999) and telemetry request.
-    fn new(throttle: u16, request_telemetry: bool) -> Self {
-        debug_assert!(throttle < 2000);
-        let translated_throttle = (throttle + 48) << 5;
-        let mut cmd = Self(translated_throttle);
+    /// Creates a new frame with the given speed (0-1999) and telemetry request.
+    ///
+    /// Returns [`None`] if the speed is out of bounds.
+    pub fn new(speed: u16, request_telemetry: bool) -> Option<Self> {
+        if speed >= 2000 {
+            return None;
+        }
+
+        let translated_throttle = (speed + 48) << 5;
+        let mut cmd = Self {
+            inner: translated_throttle,
+        };
         if request_telemetry {
-            cmd.0 |= 0x10;
+            cmd.inner |= 0x10;
         }
         cmd.compute_crc();
-        cmd
+        Some(cmd)
     }
 
-    /// Returns the throttle value (0-1999).
-    fn throttle(&self) -> u16 {
-        (self.0 >> 5) - 48
+    /// Returns the speed value (0-1999).
+    pub fn speed(&self) -> u16 {
+        (self.inner >> 5) - 48
     }
 
     /// Returns whether telemetry is enabled.
-    fn telemetry_enabled(&self) -> bool {
-        self.0 & 0x10 != 0
+    pub fn telemetry_enabled(&self) -> bool {
+        self.inner & 0x10 != 0
     }
 
     /// Returns the CRC checksum.
-    fn crc(&self) -> u16 {
-        self.0 & 0x0F
+    pub fn crc(&self) -> u16 {
+        self.inner & 0x0F
     }
 
     /// Computes the CRC based on the first 12 bits and ORs it in.
     fn compute_crc(&mut self) {
-        let value = self.0 >> 4;
+        let value = self.inner >> 4;
         let crc = (value ^ (value >> 4) ^ (value >> 8)) & 0x0F;
-        self.0 |= crc;
+        self.inner |= crc;
     }
 
     /// Returns the raw [`u16`].
-    fn inner(&self) -> u16 {
-        self.0
+    pub fn inner(&self) -> u16 {
+        self.inner
     }
 
-    /// Returns an array of booleans describing the bits.
-    fn bits(&self) -> [bool; 16] {
-        let mut value = self.0.reverse_bits();
-        let mut rv = [false; 16];
+    /// Returns an array of duty cycles for use in PWM DMA.
+    ///
+    /// This contains an extra element that is always zero to ensure the PWM output gets pulled low
+    /// at the end of the sequence. It can be sliced off if not needed.
+    pub fn duty_cycles(&self, max_duty_cycle: u16) -> [u16; 17] {
+        let mut value = self.inner.reverse_bits();
+        let mut rv = [max_duty_cycle * 3 / 4; 17];
         for item in rv.iter_mut() {
             let bit = value & 0x0001;
             if bit != 0 {
-                *item = true;
+                *item = max_duty_cycle * 3 / 8;
             }
             value >>= 1;
         }
+        rv[16] = 0;
         rv
     }
 }
@@ -247,37 +92,31 @@ mod tests {
 
     #[test]
     fn bits_works() {
-        let frame = Frame::new(999, false);
+        let frame = Frame::new(999, false).unwrap();
         assert_eq!(
-            frame.bits(),
-            [
-                true, false, false, false, false, false, true, false, true, true, true, false,
-                false, true, false, false
-            ]
+            frame.duty_cycles(100),
+            [37, 75, 75, 75, 75, 75, 37, 75, 37, 37, 37, 75, 75, 37, 75, 75, 0]
         );
     }
 
     #[test]
-    fn speed_timings() {
-        let speed = Speed::DShot600;
-        assert_eq!(speed.one_hold_time().as_nanos(), 1_250);
-        assert_eq!(speed.zero_hold_time().as_nanos(), 625);
-        assert_eq!(speed.bit_time().as_nanos(), 1_667);
-    }
-
-    #[test]
     fn frame_constructs_correctly() {
-        let frame = Frame::new(998, false);
-        assert_eq!(frame.throttle(), 998);
+        let frame = Frame::new(998, false).unwrap();
+        assert_eq!(frame.speed(), 998);
         assert!(!frame.telemetry_enabled());
         assert_eq!(frame.crc(), 0x06);
     }
 
     #[test]
     fn frame_constructs_correctly_with_telemetry() {
-        let frame = Frame::new(998, true);
-        assert_eq!(frame.throttle(), 998);
+        let frame = Frame::new(998, true).unwrap();
+        assert_eq!(frame.speed(), 998);
         assert!(frame.telemetry_enabled());
         assert_eq!(frame.crc(), 0x07);
+    }
+
+    #[test]
+    fn frame_rejects_invalid_speed_values() {
+        assert!(Frame::new(2000, false).is_none())
     }
 }
