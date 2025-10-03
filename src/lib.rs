a8553bef8af8aa7ebe30b90f69312b031ee4ad02
RaulIQ <raulalimbekov@gmail.com>
2025-10-03 17:10:57 +0300

    separate throttle encoding logic per protocol

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,7 @@
 //! );
 //! let max_duty_cycle = pwm.get_max_duty() as u16;
 //!
-//! let frame = Frame::new(1000, false).unwrap();
+//! let frame = Frame::<NormalDshot>::new(1000, false).unwrap();
 //! pwm.waveform_up(&mut dma, Ch1, &frame.duty_cycles()).await;
 //! // Pull the line low after sending a frame.
 //! pwm.set_duty(channel, 0);
@@ -40,6 +40,8 @@ pub trait DshotProtocol {
 
     /// Returns `true` if the signal is inverted (bidirectional mode).
     fn is_inverted() -> bool;
+
+    fn get_translated_throttle(speed: u16) -> u16;
 }
 
 /// Standard (non-inverted) DShot protocol.
@@ -54,6 +56,10 @@ impl DshotProtocol for NormalDshot {
     fn is_inverted() -> bool {
         false
     }
+
+    fn get_translated_throttle(speed: u16) -> u16 {
+        speed + 48 << 5
+    }
 }
 
 /// Bidirectional (inverted) DShot protocol.
@@ -67,6 +73,11 @@ impl DshotProtocol for BidirectionalDshot {
 
     fn is_inverted() -> bool {
         true
+    }
+
+    fn get_translated_throttle(speed: u16) -> u16 {
+        let mask = 0b111_1111_1111;
+        (!(speed + 48) & mask) << 5
     }
 }
 
@@ -84,14 +95,14 @@ impl<P: DshotProtocol> Frame<P> {
     ///
     /// ```
     /// # use dshot_frame::*;
-    /// assert_eq!(Frame::new(1000, false).unwrap().speed(), 1000);
+    /// assert_eq!(Frame::<NormalDshot>(1000, false).unwrap().speed(), 1000);
     /// ```
     pub fn new(speed: u16, request_telemetry: bool) -> Option<Self> {
         if speed >= 2000 {
             return None;
         }
 
-        let translated_throttle = (speed + 48) << 5;
+        let translated_throttle = P::get_translated_throttle(speed);
         let mut frame = Self {
             inner: translated_throttle,
             _protocol: core::marker::PhantomData,
@@ -371,5 +382,28 @@ mod tests {
     #[test]
     fn frame_rejects_invalid_speed_values() {
         assert!(NormalFrame::new(2000, false).is_none())
+    }
+}
+
+
+#[cfg(test)]
+mod bidi_and_telemetry_tests {
+    use super::*;
+
+    const MAX_DUTY_CYCLE: u16 = 100;
+    const ZERO: u16 = 37;
+    const ONE: u16 = 75;
+
+    #[test]
+    fn duty_cycles_works() {
+        // let frame = BidirectionalFrame::new(999, true).unwrap();
+        let thr = BidirectionalDshot::get_translated_throttle(999);
+        assert_eq!(thr, 0b011_1110_1000_00000)
+        // assert_eq!(
+        //     frame.duty_cycles(MAX_DUTY_CYCLE),
+        //     [
+        //         ZERO, ONE, ONE, ONE, ONE, ONE, ZERO, ONE, ZERO, ZERO, ZERO, ZERO, ONE, ONE, ONE, ZERO, 0
+        //     ]
+        // );
     }
 }
